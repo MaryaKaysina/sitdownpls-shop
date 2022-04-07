7b9ad69231c323a75617ee7bf787a650e3961dec
Marya Kaysina <linkinpark335@gmail.com>
2022-04-07 11:45:03 +0500

    created couch page

--- src/js/components/catalogCards.js
+++ src/js/components/catalogCards.js
@@ -4,26 +4,180 @@ if (catalog) {
   const cardsList = JSON.parse(localStorage.getItem('cards'));
   const cardsListCatalog = document.querySelector('.card__list');
   const addCardsBtn = document.querySelector('.catalog__btns');
+  const btns = document.querySelectorAll('.btns__item');
+  const filters = document.querySelectorAll('.filter__wrap input');
+  const filterPrice = document.querySelector('.input__price--max');
+  const search = document.querySelector('.header__search');
+  const searchBtn = document.querySelector('.header__btn-search');
+  let newCardList = [];
+  let filterList = [];
 
-  function checkWidth() {
-    let width = window.innerWidth;
-    addCardsBtn.classList.remove('btn--hide');
-    if (width > 1270) renderCards(cardsList, cardsList.length, 9, cardsListCatalog);
-    if (width <= 1270) renderCards(cardsList, cardsList.length, 6, cardsListCatalog);
+  searchBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+  });
 
-    const cards = document.querySelectorAll('.rating .card__wrap');
+  search.addEventListener('change', (e) => {
+    e.preventDefault();
+    searchCards(search.value);
+  });
 
-    // addCardsBtn.addEventListener('click', () => {
-    //   cards.forEach(card => {
-    //     card.classList.remove('card__wrap--hide');
-    //   });
-    //   addCardsBtn.classList.add('btn--hide');
-    // });
+  function searchCards(str) {
+    let filterListSearch = [];
+    if(str) {
+      filterListSearch.push(cardsList.filter(card => card.title.toLowerCase().indexOf(str.toLowerCase()) > -1));
+      let searchStr = filterListSearch.flat();
+      if(searchStr.length !== 0) {
+        checkWidth(filterListSearch);
+      }
+    } else {
+      checkWidth(cardsList);
+    }
   }
 
-  checkWidth();
+  function filterCards(cardsList, filterList) {
+    let filterListCategory = [];
+    let filterListSale = [];
+    let filterListColor = [];
+    let filterListPrice = [];
+    newCardList = [];
 
-  window.addEventListener('resize', () => {
-    checkWidth();
-  });
+    filterList.forEach(filter => {
+      filterListCategory.push(cardsList.filter(card => card.category === filter));
+      filterListSale.push(cardsList.filter(card => card.sale === filter));
+      filterListColor.push(cardsList.filter(card => card.color === filter));
+      filterListPrice.push(cardsList.filter(card => parseInt(card.price.replace(' ',''), 10) <= parseInt(filter, 10)));
+    });
+
+    let category = filterListCategory.flat();
+    let sale = filterListSale.flat();
+    let color = filterListColor.flat();
+    let price = filterListPrice.flat();
+
+    let maxArr = Math.max(category.length, sale.length, color.length, price.length);
+
+    if (category.length === maxArr) {
+      newCardList = category;
+      if (color.length !== 0) {
+        newCardList = newCardList.filter(i => color.includes(i));
+      }
+      if (sale.length !== 0) {
+        newCardList = newCardList.filter(i => sale.includes(i));
+      }
+      if (price.length !== 0) {
+        newCardList = newCardList.filter(i => price.includes(i));
+      }
+    }
+
+    if (color.length === maxArr) {
+      newCardList = color;
+      if (category.length !== 0) {
+        newCardList = newCardList.filter(i => category.includes(i));
+      }
+      if (sale.length !== 0) {
+        newCardList = newCardList.filter(i => sale.includes(i));
+      }
+      if (price.length !== 0) {
+        newCardList = newCardList.filter(i => price.includes(i));
+      }
+    }
+
+    if (sale.length === maxArr) {
+      newCardList = sale;
+      if (color.length !== 0) {
+        newCardList = newCardList.filter(i => color.includes(i));
+      }
+      if (category.length !== 0) {
+        newCardList = newCardList.filter(i => category.includes(i));
+      }
+      if (price.length !== 0) {
+        newCardList = newCardList.filter(i => price.includes(i));
+      }
+    }
+
+    if (price.length === maxArr) {
+      newCardList = price;
+      if (color.length !== 0) {
+        newCardList = newCardList.filter(i => color.includes(i));
+      }
+      if (sale.length !== 0) {
+        newCardList = newCardList.filter(i => sale.includes(i));
+      }
+      if (category.length !== 0) {
+        newCardList = newCardList.filter(i => category.includes(i));
+      }
+    }
+
+    return newCardList;
+  }
+
+  function filterCheck() {
+    filterList = [];
+
+    filters.forEach(filter => {
+      if(filter.checked) {
+        filterList.push(filter.value);
+        filterList.push(filterPrice.value);
+      }
+    })
+
+    if(filterList.length !== 0) {
+      let filterCardsList = filterCards(cardsList, filterList);
+      checkWidth(filterCardsList);
+    } else {
+      newCardList = [];
+      checkWidth(cardsList);
+    }
+  }
+
+  function checkWidth(list) {
+    let width = window.innerWidth;
+
+    if (width > 1270) {
+      renderCards(list, list.length, 9, cardsListCatalog);
+
+      btns.forEach(btn => {
+        btn.classList.add('btn--hide');
+        if (list.length / 9 >= 1) btn.classList.remove('btn--hide');
+      });
+    };
+    if (width <= 1270) {
+      renderCards(list, list.length, 6, cardsListCatalog);
+
+      btns.forEach(btn => {
+        btn.classList.add('btn--hide');
+        if (list.length / 6 >= 1) btn.classList.remove('btn--hide');
+      });
+    };
+
+    const cards = document.querySelectorAll('.catalog .card__wrap');
+
+    addCardsBtn.addEventListener('click', (event) => {
+      cards.forEach(card => {
+        if(card.classList.contains('card__wrap--hide')) {
+          card.classList.remove('card__wrap--hide');
+        } else {
+          card.classList.add('card__wrap--hide');
+        }
+      });
+
+      let targetBtn = event.target;
+      btns.forEach(e => {
+        e.classList.remove('is-active');
+      });
+      if(targetBtn.parentElement.classList.contains('btns__item')) {
+        targetBtn.parentElement.classList.add('is-active');
+      }
+      if(targetBtn.classList.contains('btns__item')) {
+        targetBtn.classList.add('is-active');
+      }
+    });
+  }
+
+  filterCheck();
+
+  filters.forEach(filter => {
+    filter.addEventListener('change', () => {
+      filterCheck();
+    })
+  })
 }
---
+++ src/js/components/filters.js
@@ -0,0 +1,205 @@
+$( function() {
+  const containerTags = document.querySelector('.catalog__tags');
+  const filters = document.querySelectorAll('.filter__wrap input');
+  const filterCategory = document.querySelectorAll('.filter__checkbox--category');
+  const filterSale = document.querySelectorAll('.filter__checkbox--sale');
+  const filterColor = document.querySelectorAll('.filter__checkbox--color');
+  const filterInputs = document.querySelectorAll('.input__price');
+  const sliderRange = document.getElementById('slider-range');
+  const sliderHandle = document.querySelectorAll('.ui-slider-handle');
+  const minPrice = document.querySelector('.input__price--min');
+  const maxPrice = document.querySelector('.input__price--max');
+
+  function changeMaxPrice() {
+    let addTagsPrice = [];
+    const oldTagsPrice = document.querySelectorAll('.tag__item--price');
+
+    oldTagsPrice.forEach( e => e.remove() );
+
+    if(maxPrice.value > 0) {
+      addTagsPrice.push(`До ${maxPrice.value}`);
+
+      let newTagAddPrice = document.createElement('li');
+      newTagAddPrice.classList.add('tag__item');
+      newTagAddPrice.classList.add('tag__item--price');
+      newTagAddPrice.innerHTML = `<span>${addTagsPrice}</span>
+        <button class="tag__close"></button>`;
+
+      containerTags.append(newTagAddPrice);
+
+      const tagClose = document.querySelectorAll('.tag__close');
+      let text = '';
+
+      tagClose.forEach(btn => {
+        btn.addEventListener('click', (event) => {
+          event.preventDefault();
+          text = event.target.parentElement.innerText;
+          if (text.substring(0, 3) === 'До ') {
+            event.target.parentElement.classList.add('is-hide');
+            maxPrice.value = 240000;
+            const width = (maxPrice.value - parseInt(minPrice.value, 10)) * 100 / maxWidth;
+            $('.ui-slider-handle')[1].style.cssText = `left: 100%;`;
+            $('.ui-slider-range')[0].style.cssText = `left: calc(100% - ${width}%); width: ${width}%`;
+          }
+        })
+      })
+    }
+  }
+
+  function createTags() {
+    let addTagsCategory = [];
+    let addTagsSale = [];
+    let addTagsColor = [];
+
+    containerTags.innerHTML = '';
+
+    // category tags
+    filterCategory.forEach(check => {
+      if(check.childNodes[1].hasAttribute('checked') || check.childNodes[1].checked) {
+        addTagsCategory.push(check.childNodes[1].value);
+      };
+    })
+
+    addTagsCategory.forEach(e => {
+      let newTagAddCategory = document.createElement('li');
+      newTagAddCategory.classList.add('tag__item');
+      newTagAddCategory.classList.add('tag__item--category');
+      newTagAddCategory.innerHTML = `<span>${e}</span>
+        <button class="tag__close"></button>`;
+      containerTags.append(newTagAddCategory);
+    })
+
+    // price tags
+    sliderRange.addEventListener('click', () => {
+      changeMaxPrice();
+    })
+
+    sliderHandle.forEach(e => {
+      e.addEventListener('click', () => {
+        changeMaxPrice();
+      })
+    })
+
+    maxPrice.addEventListener('change', () => {
+      changeMaxPrice();
+    })
+
+    // sale tags
+    filterSale.forEach(check => {
+      if(check.childNodes[1].hasAttribute('checked') || check.childNodes[1].checked) {
+        addTagsSale.push(check.childNodes[1].value);
+      };
+    })
+
+    addTagsSale.forEach(e => {
+      let newTagAddSale = document.createElement('li');
+      newTagAddSale.classList.add('tag__item');
+      newTagAddSale.classList.add('tag__item--sale');
+      newTagAddSale.innerHTML = `<span>${e}</span>
+        <button class="tag__close"></button>`;
+      containerTags.append(newTagAddSale);
+    })
+
+    // color tags
+    filterColor.forEach(check => {
+      if(check.childNodes[1].hasAttribute('checked') || check.childNodes[1].checked) {
+        addTagsColor.push(check.childNodes[1].value);
+      };
+    })
+
+    addTagsColor.forEach(e => {
+      let newTagAddColor = document.createElement('li');
+      newTagAddColor.classList.add('tag__item');
+      newTagAddColor.classList.add('tag__item--color');
+      newTagAddColor.innerHTML = `<span>${e}</span>
+        <button class="tag__close"></button>`;
+      containerTags.append(newTagAddColor);
+    })
+  }
+
+  function deleteTags() {
+    const tagClose = document.querySelectorAll('.tag__close');
+    let text = '';
+
+    tagClose.forEach(btn => {
+      btn.addEventListener('click', (event) => {
+        event.preventDefault();
+        text = event.target.parentElement.innerText;
+        filters.forEach(e => {
+          if (text.trim() === e.value.trim()) {
+            event.target.parentElement.classList.add('is-hide');
+            e.checked = false;
+          }
+        })
+      })
+    })
+  }
+
+  createTags();
+  deleteTags();
+
+  filters.forEach(filter => {
+    filter.addEventListener('change', () => {
+      createTags();
+      changeMaxPrice();
+      deleteTags();
+    })
+  })
+
+  const minWidth = 0;
+  const maxWidth = 240000;
+
+  minPrice.value = 2000;
+  maxPrice.value = 150000;
+
+  filterInputs.forEach(e => e.addEventListener('change', () => {
+    let filterInputsIntMin = parseInt(filterInputs[0].value, 10);
+    let filterInputsIntMax = parseInt(filterInputs[1].value, 10);
+
+    if (filterInputsIntMin === filterInputsIntMax || filterInputsIntMin > filterInputsIntMax) {
+      filterInputs[0].value = Math.min(filterInputsIntMin, maxWidth);
+      filterInputs[1].value = filterInputs[0].value;
+      const left = filterInputs[1].value * 100 / maxWidth;
+      $('.ui-slider-handle')[0].style.cssText = `left: ${left}%;`;
+      $('.ui-slider-handle')[1].style.cssText = `left: ${left}%;`;
+      $('.ui-slider-range')[0].style.cssText = `left: 0%; width: 0%`;
+    };
+    if (e.value <= minWidth) {
+      e.value = minWidth;
+      const width = filterInputs[1].value * 100 / maxWidth;
+      $('.ui-slider-handle')[0].style.cssText = `left: 0%;`;
+      $('.ui-slider-range')[0].style.cssText = `left: 0%; width: ${width}%`;
+    };
+    if (e.value >= maxWidth) {
+      e.value = maxWidth;
+      const width = filterInputs[1].value * 100 / maxWidth;
+      $('.ui-slider-handle')[1].style.cssText = `left: 100%;`;
+      $('.ui-slider-range')[0].style.cssText = `left: calc(100% - ${width}%); width: ${width}%`;
+      if(Math.min(filterInputsIntMin, maxWidth) === maxWidth) {
+        $('.ui-slider-range')[0].style.cssText = `left: 0%; width: 0%`;
+      }
+    };
+    if (filterInputsIntMin < filterInputsIntMax) {
+      const width = (filterInputs[1].value - filterInputs[0].value) * 100 / maxWidth;
+      const left = filterInputs[0].value * 100 / maxWidth;
+      $('.ui-slider-handle')[0].style.cssText = `left: ${left}%;`;
+      $('.ui-slider-handle')[1].style.cssText = `left: ${left + width}%;`;
+      $('.ui-slider-range')[0].style.cssText = `left: ${left}%; width: ${width}%`;
+    };
+  }))
+
+  $( "#slider-range" ).slider({
+    range: true,
+    min: minWidth,
+    max: maxWidth,
+    step: 1000,
+    values: [ 2000, 150000 ],
+    slide: function( event, ui ) {
+      $( ".input__price--min" ).val( ui.values[ 0 ] );
+      $( ".input__price--max" ).val( ui.values[ 1 ] );
+    }
+  });
+
+  $( ".input__price--min" ).val( ui.values[ 0 ] ).slider( "values", 0 );
+  $( ".input__price--max" ).val( ui.values[ 1 ] ).slider( "values", 1 );
+});
